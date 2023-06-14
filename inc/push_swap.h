bbe37d9995193faa7e2150ed1e4303a46018e048
Mireia Navarro Navarro <mirnavar@cbr2s5.42barcelona.com>
2023-06-14 18:17:33 +0200

    push_swap

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mirnavar <mirnavar@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 16:39:17 by mirnavar          #+#    #+#             */
-/*   Updated: 2023/06/14 17:54:34 by mirnavar         ###   ########.fr       */
+/*   Updated: 2023/06/14 18:17:01 by mirnavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,5 @@
 typedef struct s_elems
 {
 	int		data;
-	int
+	int		index;
 }
