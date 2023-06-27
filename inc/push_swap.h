13d2594b6b897baf8493a31c9003050a915986a8
Mireia Navarro Navarro <mirnavar@cbr2s1.42barcelona.com>
2023-06-27 11:14:25 +0200

    push_swap

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mirnavar <mirnavar@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 16:39:17 by mirnavar          #+#    #+#             */
-/*   Updated: 2023/06/14 18:18:43 by mirnavar         ###   ########.fr       */
+/*   Updated: 2023/06/16 11:49:58 by mirnavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,33 @@
   // aux node//
 typedef struct s_elems
 {
-	int		data;
-	int		index;
-
+	int				val;
+	int				index;
+	struct s_elems	*next;
+	struct s_elems	*prev;
+	t_elems;	
 }
 
+// stack structure //
+typedef s_stack
+{
+	int				size;
+	t_node			top;
+	t_node			bott;
+	t_stack;
+}
+
+// ----------------- FUNCTIONS ---------------- //
+// Checker: src/check.c                         //
+
+
+// Instruction Moves:                          //
+						/*  mov/swap.c  */
+
+						/*  mov/push.c  */
+
+						/*  mov/rotate.c  */
+
+						/*  mov/rev_rotate.c  */
+
+// 
